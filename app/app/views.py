5066e371f39623caa46dd150da4716a148c6cf5d
vdemare76 <57753397+vdemare76@users.noreply.github.com>
2020-02-25 16:42:23 +0100

    Update views.py

--- app/app/views.py
+++ app/app/views.py
@@ -4,82 +4,48 @@ from flask_appbuilder.models.sqla.interface import SQLAInterface
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
 
 from . import appbuilder, db
-from .models import AnniAccademici, CorsiDiStudio, AttivitaDidattiche, Docenti, Aule, OffertaDidattica
+from .models import AnniAccademici, CorsiDiStudio, AttivitaDidattiche, Docenti, Aule, OffertaDidattica, LogisticaDocenti
 
 class AnniAccademiciView(ModelView):
     datamodel = SQLAInterface(AnniAccademici)
-    # base_permissions = ['can_add', 'can_show']
     list_columns = ["anno", "anno_esteso"]
 
 class CorsiDiStudioView(ModelView):
     datamodel = SQLAInterface(CorsiDiStudio)
-    # base_permissions = ['can_add', 'can_show']
     list_columns = ["codice", "descrizione", "cfu", "durata_legale"]
 
 class AttivitaDidatticheView(ModelView):
     datamodel = SQLAInterface(AttivitaDidattiche)
-    # base_permissions = ['can_add', 'can_show']
     list_columns = ["codice", "descrizione", "cfu"]
 
 class AuleView(ModelView):
     datamodel = SQLAInterface(Aule)
-    # base_permissions = ['can_add', 'can_show']
     list_columns = ["codice", "descrizione", "capienza", "tipo_aula"]
 
 class DocentiView(ModelView):
     datamodel = SQLAInterface(Docenti)
-    # base_permissions = ['can_add', 'can_show']
     list_columns = ["codice_fiscale", "cognome", "nome"]
 
 class OffertaDidatticaView(ModelView):
     datamodel = SQLAInterface(OffertaDidattica)
-    # base_permissions = ['can_add', 'can_show']
     list_columns = ["anno_accademico", "corso_di_studio", "attivita_didattica", "docente", "anno_di_corso", "max_studenti", "semestre"]
-    # related_views = [AnniAccademiciView]
 
-""" 
-class EmployeeView(ModelView):
-    datamodel = SQLAInterface(Employee)
-
-    list_columns = ["full_name", "department.name", "employee_number"]
-    edit_form_extra_fields = {
-        "department": QuerySelectField(
-            "Department",
-            query_factory=department_query,
-            widget=Select2Widget(extra_classes="readonly"),
-        )
-    }
-
-    related_views = [EmployeeHistoryView]
-    show_template = "appbuilder/general/model/show_cascade.html"
-
-
-class FunctionView(ModelView):
-    datamodel = SQLAInterface(Function)
-    related_views = [EmployeeView]
-
-
-class BenefitView(ModelView):
-    datamodel = SQLAInterface(Benefit)
-    add_columns = ["name"]
-    edit_columns = ["name"]
-    show_columns = ["name"]
-    list_columns = ["name"] """
+class LogisticaDocentiView(ModelView):
+    datamodel = SQLAInterface(LogisticaDocenti)
+    list_columns = ["offerta", "durata_sessioni", "numero_sessioni"]
 
 db.create_all()
 
-# appbuilder.add_view_no_menu(EmployeeHistoryView, "EmployeeHistoryView")
 appbuilder.add_view(
     AnniAccademiciView, "Anni accademici", icon="fa-font"
 )
-# appbuilder.add_separator("Company")
 
 appbuilder.add_view(
     CorsiDiStudioView, "Corsi di studio", icon="fa-pencil"
 )
 
 appbuilder.add_view(
-    AttivitaDidatticheView, "Attivita didattiche", icon="fa-pencil-square-o"
+    AttivitaDidatticheView, "Attività didattiche", icon="fa-pencil-square-o"
 )
 
 appbuilder.add_view(
@@ -92,4 +58,8 @@ appbuilder.add_view(
 
 appbuilder.add_view(
     OffertaDidatticaView, "Offerta didattica", icon="fa-map"
+)
+
+appbuilder.add_view(
+    LogisticaDocentiView, "Logistica docenti", icon="fa-wrench"
 )
