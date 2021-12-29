b29e1c486d6c3054d975d0a0f211edc26d625537
vdemare76 <vincenzo.demare@studenti.uniparthenope.it>
2021-12-30 00:03:35 +0100

    Update

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -10,6 +10,7 @@ logging.basicConfig(format="%(asctime)s:%(levelname)s:%(name)s:%(message)s")
 logging.getLogger().setLevel(logging.DEBUG)
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '\2\1thisismyscretkey\1\2\e\y\y\h'
 app.config['SESSION_PERMANENT'] = True
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(hours=5)
